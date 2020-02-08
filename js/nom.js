98ce2f7844b1e64c0335c160f4035469cf5189b9
Antoine FSG <35838884+AntoineFSG@users.noreply.github.com>
2020-02-08 13:24:13 -0800

    Update nom.js

--- js/nom.js
+++ js/nom.js
@@ -1,25 +1,31 @@
 function shuffle(){
 
 var nom = ['Arko','Bradibou','Cosmog','Darkrai','Evoli','Feunnec','Germignon','Hippodocus','Iguolta','Judocrack','Kabuto','Lunala','Macogneur','N\351crosma','Otaquin','Picachu','Qulbutoquet','Rocaillou','Sorb\351b\351','Tutafeh','Ursaring','Vip\351lierre','Wattouat','Xatu','Yveltal','Zygarde'];
-
-var str = "<div class='flex-ul'>";
-
 var randomNumber = Math.floor(Math.random() * 26);
 var randomNom = nom[randomNumber];
+var str = `<div class='flex-ul'>
+                <div class='flip-container'>
+                    <div class='card' id='${randomNom}'>
+                        <div class='front face'>
+                            <div class='full-face'>
+                                <p>${randomNom}</p>
+                            </div>
+                        </div>
+                        <div class='back face' id='back'>
+                            <div class='left-side'>
+                                <p class='big'>${randomNom.charAt(0)}</p>
+                            </div>
+                            <div class='img-container'>
+                                <img class='liste-img' src='img/back/${randomNom.charAt(0)}.jpg'>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+
+
 
 console.log(randomNom);
-
-  str +="<div class='flip-container'>";
-  str +="<div class='card' id='"+ randomNom +"''>";
-  str +="<div class='front face'>";
-  str +="<div class='full-face'><p>"+ randomNom +"</p></div>";
-  str +="</div>";
-  str +="<div class='back face' id='back'>";
-  str +="<div class='left-side'><p class='big'>"+ randomNom.charAt(0) +"</p></div>";
-  str +="<div class='img-container'><img class='liste-img' src='img/back/"+ randomNom.charAt(0) +".jpg'></div>";
-  str += "</div></div></div>"
-  str += '</div>';
-
 
 document.getElementById("listeLettres").innerHTML = str;
 
